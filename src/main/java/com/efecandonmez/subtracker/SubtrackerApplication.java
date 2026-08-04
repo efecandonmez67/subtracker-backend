063d254e6b206b9c56a9b6bd7a64d4999054ff68
efecandonmez67 <ecdonmez67@gmail.com>
2026-08-05 00:09:37 +0300

    initial commit: Setup Spring Boot skeleton and database connection

---
+++ src/main/java/com/efecandonmez/subtracker/SubtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.efecandonmez.subtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efecandonmez/subtracker/SubtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efecandonmez.subtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
